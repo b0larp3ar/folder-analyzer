5dc210c181649c76bd769ae11dab64c86e475359
b0larp3ar <148764066+b0larp3ar@users.noreply.github.com>
2026-06-02 09:08:58 +0530

    File type count feature, cleaned code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 units=["B" ,"KB", "MB", "GB", "TB"]
 
-def formatSize(size):
+def convertBytes(size):
     if size==0:
         return "0 B"
     
@@ -14,10 +14,9 @@ def formatSize(size):
 
     return f"{size:.2f} {units[i]}"
 
-def analyzeFolder(path):
+#number of files and folders, total size
 
-    #number of files and folders, total size
-
+def basicInfo(path):
     nfolders = 0
     nfiles = 0
     size=0
@@ -33,8 +32,9 @@ def analyzeFolder(path):
     print(f"Number of files: {nfiles}")
     print(f"Total size: {size}")
 
-    #sorting based on file size
+#sorting based on file size
 
+def sortSize(path):
     array=[]
 
     for root, dirs, files in os.walk(path):
@@ -46,21 +46,54 @@ def analyzeFolder(path):
     array.sort(key=lambda x:x[1], reverse=True)
     print(array)
 
-    #converting bytes to kb,mb,gb,etc.
+#converting bytes to kb,mb,gb,etc.
 
+def formatSize(path):
     array=[]
 
     for root, dirs, files in os.walk(path):
         for i in files:
-            p=os.path.join(root, i)
+            p=os.path.join(root, i) 
             s=os.path.getsize(p)
             array.append((p, s))
 
     array.sort(key=lambda x:x[1], reverse=True)
 
     for p, s in array:
-        print(f"{formatSize(s):>20}  {p}")
+        print(f"{convertBytes(s):>20}  {p}")
 
+#File type count
+
+def fileTypeCount(path):
+    dic={}
+
+    for root, dirs, files in os.walk(path):
+        for i in files:
+            type=os.path.splitext(i)[1]
+            if type=="":
+                type="[No extension]"
+            if type in dic:
+                dic[type]+=1
+            else:
+                dic[type]=1
+
+    dic=dict(sorted(dic.items(), key=lambda x:x[1], reverse=True))
+
+    print("\nFile Types:\n")
+
+    for extension, count in dic.items():
+        print(f"{extension:<15} {count}")
+
+#analyze folder
+
+def analyzeFolder(path):
+    basicInfo(path)
+    sortSize(path)
+    formatSize(path)
+    fileTypeCount(path)
+
+#main
 
 path=str(input("Enter absolute path of the folder: "))
 analyzeFolder(path)
+
